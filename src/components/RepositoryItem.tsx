c85e1ae449ec3f23425eba9e70966e39b227bb16
SantiGO055 <santigonzalez05@gmail.com>
2022-08-25 17:57:37 -0300

    .

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,13 @@
+const RepositoryItem = (props) => {
+    console.log(props)
+    return (
+        // <FlatList
+        //     data={repositories}
+        //     ItemSeparatorComponent={ItemSeparator}
+        //     renderItem={RepositoryItem} keyExtractor={item => item.id}
+        // // other props
+        // />
+    );
+};
+
+export default RepositoryItem;
